589c95aea5e75d2c39630ad24dd5b7db8240c5f2
Terranie Sims <terranie67@gmail.com>
2020-03-27 17:03:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,9 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["one", "two", "three", "four"]
+  array = ["wow","I", "am", "really", "learning", "arrays!"]
+  index_number = 2
+  array.index[2]
 end
 
 def retrieve_first_element_from_array(array)
