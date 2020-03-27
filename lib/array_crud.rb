50cf092a5df36865fd8202b98869d03fd4e78214
Terranie Sims <terranie67@gmail.com>
2020-03-27 16:48:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["one", "two", "three", "four"]
+  element = "five"
+  array << element 
 end
 
 def add_element_to_start_of_array(array, element)
