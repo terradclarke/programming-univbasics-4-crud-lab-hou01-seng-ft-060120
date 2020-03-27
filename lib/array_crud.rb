7967bfad4a2bf93b8c54a803343aef44fa9438b6
Terranie Sims <terranie67@gmail.com>
2020-03-27 16:59:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ def create_an_array
   ["one", "two", "three", "four"]
 end
 
-def add_element_to_end_of_array($array, element)
+def add_element_to_end_of_array(array, element)
   array = "I", "am", "really", "learning"
   element = "arrays!"
   array << element 
