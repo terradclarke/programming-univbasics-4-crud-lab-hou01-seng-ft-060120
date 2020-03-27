59afecd063a3d88ffcdfae1b0a0dc6994e686774
Terranie Sims <terranie67@gmail.com>
2020-03-27 16:51:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,9 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["one", "two", "three", "four"]
+  element = "wow"
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
