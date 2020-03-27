a910e620cc93e28f85c0ca6b995e17e3f62f57f8
Terranie Sims <terranie67@gmail.com>
2020-03-27 17:14:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,8 @@ end
 def retrieve_element_from_index(array, index_number)
   array = ["wow","I", "am", "really", "learning", "arrays!"]
   index_number = 2
-  p array.index(index_number)
+  index = array.index(index_number)
+  p index 
 end
 
 def retrieve_first_element_from_array(array)
