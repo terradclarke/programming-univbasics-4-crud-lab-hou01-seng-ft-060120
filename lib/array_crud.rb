32b0873ede3cee9d12e61d7486e993382a341e35
Terranie Sims <terranie67@gmail.com>
2020-03-27 17:10:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -41,6 +41,7 @@ end
 
 def retrieve_last_element_from_array(array)
   array = ["wow","I", "am", "really", "learning", "arrays!"]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
