9a31699e13aab2a472fae29d0e8d7001eaadc315
Terranie Sims <terranie67@gmail.com>
2020-03-27 17:00:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,13 +7,13 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = "I", "am", "really", "learning"
+  array = ["I", "am", "really", "learning"]
   element = "arrays!"
   array << element 
 end
 
 def add_element_to_start_of_array(array, element)
-  array = ["wow", "I", "am", "really", "learning"]
+  array = ["I", "am", "really", "learning", "arrays!"]
   element = "wow"
   array.unshift(element)
 end
