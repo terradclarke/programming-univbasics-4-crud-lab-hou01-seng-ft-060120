703e1cc67991e82dd5ea023cb70fc3754570ce76
Terranie Sims <terranie67@gmail.com>
2020-03-27 17:01:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["one", "two", "three", "four"]
+  array = ["wow","I", "am", "really", "learning", "arrays!"]
   array.pop
 end
 
